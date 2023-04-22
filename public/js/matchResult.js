07c36267cb091b8142e6c1d33dafb0d2fc159bdf
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-22 01:15:10 -0700

    add fetch routes for POST and GET requests

--- public/js/matchResult.js
+++ public/js/matchResult.js
@@ -2,9 +2,15 @@
 const addFriendBtn = document.getElementById('add-friend');
 const findNewFriendBtn = document.getElementById('find-new');
 
+//create an array with five randomly selected objects
+function randomArray(arr) {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const item = arr[randomIndex];
+}
+
 //POST - add new friend
 function addNewFriend() {
-    fetch ('/api/friends', {
+    fetch ('/api/users', {
         method: 'POST',
         body: JSON.stringify({
             id,
@@ -17,7 +23,15 @@ function addNewFriend() {
     .catch(error => console.log(error))
 }
 
-//GET - get all users to put in modal
-function getNewFriends() {}
+//GET - randomly select five users to put in modal
+function getNewFriends() {
+    fetch('/api/users')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+}
 
 // click events
