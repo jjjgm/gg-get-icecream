09649cf346a4fdeff4d1a66016f39b9cb82fc0e0
jjjgm <businessjgm@icloud.com>
2023-04-23 14:23:39 -0700

    withAuth

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+//AUTHENTICATION FOR USER LOGGED IN
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    }  else {
+        next ();
+    }
+}
+
+module.exports = withAuth;
