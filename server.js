503befaa0b69b4cb47dc5924ff8659da2c13dfea
jjjgm <businessjgm@icloud.com>
2023-04-24 11:25:45 -0700

    need to resolve conflicts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const http = require('http');
-const routes = require('./controllers/');
+const routes = require('./controllers/api/apiRoutes');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
