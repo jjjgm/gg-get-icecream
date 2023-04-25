495a915697ef13fbfe5f3c1640d1cebacd0a40a0
Michrira <Michaelchristianrader@gmail.com>
2023-04-24 21:51:46 -0700

    stashing

--- models/messages.js
+++ models/messages.js
@@ -1,44 +1,20 @@
 // models/messages.js
-const { Model, DataTypes } = require('sequelize');
+
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-class Messages extends Model {}
-
-Messages.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+const Message = sequelize.define('Message',
+{
   text: {
     type: DataTypes.STRING,
     allowNull: false,
-  },
-  dogId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'dog',
-      id: 'id'
-    }
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'user',
-      id: 'id'
-    }
   }
-  
-},
-{
-  sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'messages',
 });
+//   sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'message',
+// });
 
-module.exports = Messages;
+module.exports = Message;
